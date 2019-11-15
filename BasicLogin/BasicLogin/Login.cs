9d477f53cacf12a164f53bd2dedd5dc4408dfb04
mario blagojevic <Maki97_2@hotmail.com>
2019-11-15 10:37:13 +0100

    Upload

---
+++ BasicLogin/BasicLogin/Login.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BasicLogin
+{
+    class Login
+    {
+        //decalre properties 
+        public string Username { get; set; }
+        public string Userpassword { get; set; }
+
+        //intialise  
+        public Login(string user, string pass)
+        {
+            this.Username = user;
+            this.Userpassword = pass;
+        }
+        //clear user inputs 
+        private void ClearTexts(string user, string pass)
+        {
+            user = String.Empty;
+            pass = String.Empty;
+        }
+        public bool IsLoggedIn(string user, string pass)
+        {
+            //check user name empty 
+            if (string.IsNullOrEmpty(user))
+            {
+                MessageBox.Show("Unesite korisnicko ime");
+                return false;
+
+            }
+
+            if (Username != user)
+            {
+                MessageBox.Show("Korisnicko ime je pogresno");
+                ClearTexts(user, pass);
+                return false;
+            }
+            //check password is empty 
+            else
+            {
+                if (string.IsNullOrEmpty(pass))
+                {
+                    MessageBox.Show("Uneiste lozinku");
+                    return false;
+                }
+                
+                //check password is correct 
+                else if (Userpassword != pass)
+                {
+                    MessageBox.Show("Lozinka nije ispravnja");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+        }
+    }
+}
---
+++ BasicLogin/BasicLogin/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OtkaziBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        Login login = new Login("admin", "1234");
+        private void UlogujBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string user = korisnickoImeBox.Text;
+            string pass = passBox.Password;
+
+            if (login.IsLoggedIn(user, pass))
+            {
+                MessageBox.Show("Uspesno ste se ulogovali");
+            }
+            else
+            {
+                //show default login error message 
+                MessageBox.Show("Logovanje nije uspelo");
+            }
+        }
+    }
+}
